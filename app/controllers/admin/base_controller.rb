190717b004631cf3ad2d8686d825f28a521ee5fb
Pawel Dudek <paweldominikdudek@gmail.com>
2017-07-31 21:00:27 +0200

    Implements authentication to admin panel in admin/base controller

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,2 +1,11 @@
 class Admin::BaseController < ApplicationController
+
+    layout "admin"
+    before_action :autheticate
+
+    def autheticate
+        authenticate_or_request_with_http_basic "Podaj haslo" do |name, password|
+            name == "admin" && password == "admin"
+        end
+
 end
