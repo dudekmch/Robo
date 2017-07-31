f62eb20a58ce636f2bda8e84444aaa876a36945c
Pawel Dudek <paweldominikdudek@gmail.com>
2017-07-31 22:33:58 +0200

    Implements admin/products and admin/categories controllers

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,10 +1,39 @@
 class Admin::CategoriesController < Admin::BaseController
-  def index
+    def index
+    @categories = Category.all
   end
 
   def new
+    @categories = Category.create()
+  end
+
+  def create
+      @categories=Category.create(category_params)
+    if @categories.save
+      redirect_to admin_categories_path, notice: 'Pomyslnie dodano kategorie'
+    else
+      render action: :new
+    end  
   end
 
   def edit
+    @categories = Category.find(params[:id])
+  end
+
+  def update
+    @categories = Category.find(params[:id])
+    if @categories.update_attributes(category_params)
+      redirect_to admin_categories_path, notice: 'Pomyslnie zmieniono kategorie'
+    else
+      render action: :edit
+    end
+  end
+
+private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )  
   end
 end
