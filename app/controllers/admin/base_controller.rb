010759d50d68d5ce569daaaa42b545cbf3a7e422
Pawel Dudek <paweldominikdudek@gmail.com>
2017-07-31 21:52:39 +0200

    Implements admin layout

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,11 +1,12 @@
 class Admin::BaseController < ApplicationController
 
     layout "admin"
-    before_action :autheticate
+    before_action :authenticate
 
-    def autheticate
+    def authenticate
         authenticate_or_request_with_http_basic "Podaj haslo" do |name, password|
             name == "admin" && password == "admin"
         end
+    end
 
 end
