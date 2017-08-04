e107a58d7b24f23d3a10d24e47877050c3b77fd3
Pawel Dudek <paweldominikdudek@gmail.com>
2017-08-04 19:32:17 +0200

    Validate values in models

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,4 @@
 class Address < ApplicationRecord
   belongs_to :user
+  validates :first_name, :last_name, :zip_code, :street, :city, presence: true
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ApplicationRecord
   belongs_to :shippig_type
   belongs_to :user
+  has_many :line_items
+
+  def full_cost
+    line_items.map {|e| e.full_price}.sum + shipping_cost
+  end
 end
