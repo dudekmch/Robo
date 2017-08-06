65f702340399c9b705b2caec300d60473b1797e3
Pawel Dudek <paweldominikdudek@gmail.com>
2017-08-06 11:55:48 +0200

    Configure Order model for Statesman

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,28 @@
 class Order < ApplicationRecord
+  include Statesman::Adapters::ActiveRecordQueries
   has_one :shippig_type
   belongs_to :user
   has_many :line_items
-  has_many :transitions
+  has_many :transitions, className: 'OrderTransition', autosave: false
+
+  delegate :can_transition_to?, :transition_to!, :transition_to, :current_state,
+           to: :state_machine
+
+  def state_machine
+    @state_machine ||= OrderStateMachine.new(self, transition_class: OrderTransition)
+  end
+
+  def self.transition_class
+    OrderTransition
+  end
+
+  def self.initial_state
+    OrderStateMachine.initial_state
+  end
+
+  def self.transition_name
+    :transitions
+  end
 
   def full_cost
     line_items.map { |e| e.full_price }.sum + shipping_cost
