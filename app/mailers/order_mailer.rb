477de1dcfa979bc1d3d487ddf98a4e140d8e5a87
Pawel Dudek <paweldominikdudek@gmail.com>
2017-08-06 11:08:46 +0200

    Implements order_mail

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,46 +1,21 @@
 class OrderMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.order_mailer.order_confirmation.subject
-  #
-  def order_confirmation
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def order_confirmation(order)
+    @order = order
+    mail to: order.user.email, subject: "Dziękujemy za zamówienie nr #{order.id}"
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.order_mailer.order_in_progress.subject
-  #
-  def order_in_progress
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def order_in_progress(order)
+    @order = order
+    mail to: order.user.email, subject: "Zamówienie nr #{order.id} w realizacj"
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.order_mailer.order_shipped.subject
-  #
-  def order_shipped
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def order_shipped(order)
+    @order = order
+    mail to: order.user.email, subject: "Zamówienie nr #{order.id} wysłane"
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.order_mailer.order_cancelled.subject
-  #
-  def order_cancelled
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def order_cancelled(order)
+    @order = order
+    mail to: order.user.email, subject: "Zamówienie nr #{order.id} anulowane"
   end
 end
