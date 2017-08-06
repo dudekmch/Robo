44bded4c3fe80f274491c428c7c7a7f28cd74a89
Pawel Dudek <paweldominikdudek@gmail.com>
2017-08-06 09:59:06 +0200

    Add Stateman gem with config file and model

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,9 @@ class Order < ApplicationRecord
   has_one :shippig_type
   belongs_to :user
   has_many :line_items
+  has_many :transitions
 
   def full_cost
-    line_items.map {|e| e.full_price}.sum + shipping_cost
+    line_items.map { |e| e.full_price }.sum + shipping_cost
   end
 end
