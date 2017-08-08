33f31782984710438c67bd73d2c1e980b4c43a33
Pawel Dudek <paweldominikdudek@gmail.com>
2017-08-08 22:46:09 +0200

    Add show action to addresses controller

--- app/helpers/adresses_helper.rb
+++ app/helpers/adresses_helper.rb
@@ -1,2 +1,11 @@
 module AdressesHelper
+  def full_name(address)
+    names = [address.last_name, address.first_name]
+    names.join(' ')
+  end
+
+  def city_zipcode(address)
+    data = [address.city, address.zip_code]
+    data.join(' ')
+  end
 end
