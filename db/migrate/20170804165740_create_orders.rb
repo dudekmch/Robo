faa593e7f4f06df60b36c66fe83c8142518f3a66
Pawel Dudek <paweldominikdudek@gmail.com>
2017-08-16 13:22:50 +0200

    Fix migrate

--- db/migrate/20170804165740_create_orders.rb
+++ db/migrate/20170804165740_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
-      t.belongs_to :shipping_type, foreign_key: true
+      t.belongs_to :shippig_type, foreign_key: true
       t.decimal :shipping_cost
       t.belongs_to :user, foreign_key: true
 
